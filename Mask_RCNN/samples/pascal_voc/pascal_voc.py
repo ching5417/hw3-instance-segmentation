9e5f02749b851f785df865f0d8a63d694c05b6ba
yiching <covery90172@gmail.com>
2020-12-10 15:35:55 +0000

    update gpu count

--- Mask_RCNN/samples/pascal_voc/pascal_voc.py
+++ Mask_RCNN/samples/pascal_voc/pascal_voc.py
@@ -87,7 +87,7 @@ class CocoConfig(Config):
     IMAGES_PER_GPU = 2 # 4 # 2
 
     # Uncomment to train on 8 GPUs (default is 1)
-    GPU_COUNT = 2
+    GPU_COUNT = 1
 
     # Number of classes (including background)
     NUM_CLASSES = 1 + 20  # COCO has 80 classes
